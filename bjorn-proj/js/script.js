84d80ac2eb0d5f5016ae1c341b580be1198ac35d
TrueSlavaGopnik <pablo.pashaev@mail.ru>
2022-01-28 17:05:11 +0100

    Bjorn-proj

--- bjorn-proj/js/script.js
+++ bjorn-proj/js/script.js
@@ -1,6 +1,6 @@
 $(function() {//Same as document.addEventListener("DOMContent"...
     //Same as document.querySelector("#navbarToggle").addEventListener("blur",...
-    $("#navbarToggle").blur(function(event) {
+    $(".navbar-toggle").blur(function(event) {
         var screenWidth = window.innerWidth;
         if (screenWidth < 768) {
             $("#collapsable-nav").collapse("hide");
