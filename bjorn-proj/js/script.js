361fb8ef18ea84bd3f836ade6220bd861d16756c
TrueSlavaGopnik <pablo.pashaev@mail.ru>
2022-01-29 00:36:10 +0100

    Bjorn-proj

--- bjorn-proj/js/script.js
+++ bjorn-proj/js/script.js
@@ -7,3 +7,17 @@ $(function() {//Same as document.addEventListener("DOMContent"...
         }
     });
 });
+
+(function(global) {
+    var bp = {}; //bp = Björn Proj
+
+    var homeHtml = "snippets/home-snippet.html";
+
+    //Convinience function for inserting innerHTML for 'select'
+    var insterHtml = function(selector, html) {
+        var targetElm = document.querySelector(selector);
+        tergetElm.innerHTML = html;
+    };
+
+
+})(window);
